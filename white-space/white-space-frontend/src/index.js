8c0d7a98d7ba9bb68427d68730dc1c93ed08568c
github username <github email address>
2019-07-11 18:07:35 -0500

    MVP done

--- white-space/white-space-frontend/src/index.js
+++ white-space/white-space-frontend/src/index.js
@@ -7,6 +7,41 @@ const mainRoutine = document.querySelector(".routine")
 const mainTodo = document.querySelector(".todo")
 const routineList = document.querySelector(".routineList")
 const todoList = document.querySelector(".todoList")
+const backbutton = document.createElement('button')
+const body = document.querySelector("body")
+let a;
+
+backbutton.innerText = "back"
+mainTodo.append(backbutton)
+
+backbutton.addEventListener("click", () => {
+  mainRoutine.style = "display:inline"
+  mainTodo.style = "display:none"
+  body.style.backgroundImage = "url('/Users/yutakatsuyama/flatiron/mod3_project/mod3-whitespace/background-cement-concrete-242236.jpg')"
+})
+
+
+function startTimer(duration, display) {
+  let li = display.parentNode
+   clearInterval(a)
+    let timer = duration, minutes, seconds;
+    a = setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds　+" ";
+
+        if (--timer < -1) {
+            clearInterval(a)
+            // let li = display.parentNode
+            li.remove()
+            alert("Good Job! Take a rest!")
+        }
+    }, 1000);
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -14,16 +49,36 @@ document.addEventListener('DOMContentLoaded', () => {
   .then(resp => resp.json())
   .then(data => data.forEach((routine) => {renderRoutines(routine)}))
 
+  const logo = document.querySelector(".white-logo")
+
+  logo.addEventListener("click", () => {
+    const login = document.querySelector(".login")
+    const start = document.querySelector(".routine")
+    const opening = document.querySelector(".opening")
+
+
+    login.style = "display:none"
+    opening.style = "display:none"
+    start.style = "display:inline"
+
+    body.style.backgroundImage = "url('/Users/yutakatsuyama/flatiron/mod3_project/mod3-whitespace/background-cement-concrete-242236.jpg')"
+
+
+  })
+
 function renderRoutines(routine){
 
       const li = document.createElement("li")
       const deleteButton = document.createElement("button")
+      const br = document.createElement("br")
 
-      deleteButton.innerHTML = `<img src="">`
+      // deleteButton.src = "/Users/yutakatsuyama/flatiron/mod3_project/mod3-whitespace/whitespacelogo_v1.png"
 
       li.innerText = routine.title
 
-      routineList.append(li, deleteButton)
+
+      li.append(deleteButton)
+      routineList.append(li, br)
 
       deleteButton.addEventListener("click", () => {
         let id = routine.id
@@ -37,11 +92,11 @@ function renderRoutines(routine){
 
       li.addEventListener("click", (e) => {
         let id = routine.id
-        li.style.color = "red"
+        if (e.target === deleteButton){
+        }else{
         fetch(TODOS_URL)
         .then(resp => resp.json())
-        .then(data => renderTodos(data, id))
-
+        .then(data => renderTodos(data, id))}
       })
 
 
@@ -75,6 +130,12 @@ function renderRoutines(routine){
     const newTodoName = document.querySelector("#new-todo-name")
     const newTodoDuration = document.querySelector('#new-todo-duration')
 
+    // body.style.backgroundImage = "url('/Users/yutakatsuyama/flatiron/mod3_project/mod3-whitespace/art-artist-background-316466.jpg')"
+    body.style.backgroundImage = "url('/Users/yutakatsuyama/flatiron/mod3_project/mod3-whitespace/clop-adventure-black-and-white-boat-910213.png')"
+
+
+
+
     mainRoutine.style = "display:none"
     mainTodo.style = "display:inline"
 
@@ -86,6 +147,11 @@ function renderRoutines(routine){
         duration: newTodoDuration.value
       }
 
+      if (parseInt(newTodoDuration.value) > 25){
+        alert("Too long!")
+        newTodoDuration.value=""
+      }else{
+
       fetch(TODOS_URL, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -95,30 +161,34 @@ function renderRoutines(routine){
       .then(data => {
         newTodoName.value=""
         newTodoDuration.value=""
-      })
+        renderTodo(data)
+      })}
     })
 
     todos.forEach((todo) => {
       if (id === todo.routine_id) {
         const li = document.createElement("li")
-        const h4 = document.createElement("h4")
-        const clockdiv = document.createElement("div")
-        const minutesSpan = document.createElement("span")
-        const secondsSpan = document.createElement("span")
+        const h4 = document.createElement("span")
         const deleteButton = document.createElement("button")
+        const br = document.createElement("br")
 
-        clockdiv.setAttribute("id", "clockdiv")
-        minutesSpan.className = "minutes"
-        secondsSpan.className = "seconds"
+
+
+        let id = todo.id
+
+        h4.setAttribute("id", id)
+
+        // startTimerButton.innerText = "Start"
 
         deleteButton.innerHTML = `<img src="">`
 
-        li.innerText = todo.name
-        h4.innerText = todo.duration
+        li.innerText = `${todo.name}      `
+        h4.innerText = `    ${todo.duration}:00  `
 
-        clockdiv.append(minutesSpan, secondsSpan)
 
-        li.append(h4,clockdiv, deleteButton)
+
+        // h4.append(startTimerButton)
+        li.append(h4, deleteButton)
         todoList.append(li)
 
         deleteButton.addEventListener("click", () => {
@@ -129,44 +199,81 @@ function renderRoutines(routine){
           li.remove()
           deleteButton.remove()
         })
+
+        li.addEventListener("click", () => {
+          // const otherFont = document.querySelector("html")
+          // otherFont.style.fontWeight = "lighter"
+
+          const body = document.querySelector("body")
+          li.style.background = "black"
+          li.style.color = "white"
+          li.style.padding = "5px"
+          li.style.fontSize = "80px"
+
+
+          // body.style.background =
+        let num = todo.duration
+        let todoDuration = 60 * num
+        let id = todo.id
+            display = document.getElementById(id);
+        startTimer(todoDuration, display)})
       }
       })
 
-      function renderTodo(){
-        const li = document.createElement("li")
-        const h4 = document.createElement("h4")
-        const clockdiv = document.createElement("div")
-        const minutesSpan = document.createElement("span")
-        const secondsSpan = document.createElement("span")
-        const deleteButton = document.createElement("button")
 
-        clockdiv.setAttribute("id", "clockdiv")
-        minutesSpan.className = "minutes"
-        secondsSpan.className = "seconds"
+  function renderTodo(todo){
+    const li = document.createElement("li")
+    const h4 = document.createElement("span")
+    const deleteButton = document.createElement("button")
+    const br = document.createElement("br")
 
-        deleteButton.innerHTML = `<img src="">`
 
-        li.innerText = todo.name
-        h4.innerText = todo.duration
 
-        clockdiv.append(minutesSpan, secondsSpan)
+    let id = todo.id
 
-        li.append(h4,clockdiv, deleteButton)
-        todoList.append(li)
+    h4.setAttribute("id", id)
 
-        deleteButton.addEventListener("click", () => {
-          let id = todo.id
-          fetch(TODOS_URL + `/${id}`, {
-            method: "DELETE"
-          })
-          li.remove()
-          deleteButton.remove()
-        })
+    // startTimerButton.innerText = "Start"
 
-      }
+    deleteButton.innerHTML = `<img src="">`
 
-      mainTodo.append(todoList);
+    li.innerText = `${todo.name}      `
+    h4.innerText = `    ${todo.duration}:00  `
+
+
+
+    // h4.append(startTimerButton)
+    li.append(h4, deleteButton)
+    todoList.append(li)
+
+    deleteButton.addEventListener("click", () => {
+      let id = todo.id
+      fetch(TODOS_URL + `/${id}`, {
+        method: "DELETE"
+      })
+      li.remove()
+      deleteButton.remove()
+    })
+
+    li.addEventListener("click", () => {
+      // const otherFont = document.querySelector("html")
+      // otherFont.style.fontWeight = "lighter"
+
+      const body = document.querySelector("body")
+      li.style.background = "black"
+      li.style.color = "white"
+      li.style.padding = "5px"
+      li.style.fontSize = "80px"
+
+
+      // body.style.background =
+    let num = todo.duration
+    let todoDuration = 60 * num
+    let id = todo.id
+        display = document.getElementById(id);
+    startTimer(todoDuration, display)})
   }
+}
 
 
 })
